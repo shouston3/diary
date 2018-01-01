0cb11ac8d3cfcb35f06a2c23736ed768f55fc7e2
shouston3 <shouston3@me.com>
2018-01-01 15:14:26 +0000

    setting up elm file

--- assets/elm/Home.elm
+++ assets/elm/Home.elm
@@ -1,7 +1,49 @@
 module Home exposing (..)
 
+import Html exposing (Html, text, div, h1)
 
-import Html exposing (text)
 
-main =
-    text "Hello World"
+main
+    = Html.program
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subcriptions
+    }
+
+type alias Todo =
+    { title : String
+    , description : String
+    }
+
+
+type alias Date =
+    { diary : String
+    , todos : List Todo
+    }
+
+
+type alias Model = String
+    -- { loading : Bool
+    -- , dates : Date
+    -- }
+
+initialModel : Model
+initialModel = "Hello"
+
+init : ( Model, Cmd Msg )
+init = ( "Hello", Cmd.none )
+
+view : Model -> Html Msg
+view model =
+    div [] [
+        h1 [] [ text model ]
+        ]
+
+type Msg = M
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model = ( model, Cmd.none )
+
+subcriptions : Model -> Sub Msg
+subcriptions model = Sub.none
