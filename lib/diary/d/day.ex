fe9f464466753370df609eae1c654ebde78612fd
shouston3 <shouston3@me.com>
2018-01-03 14:39:32 +0000

    correct the has_many field

--- lib/diary/d/day.ex
+++ lib/diary/d/day.ex
@@ -1,13 +1,15 @@
 defmodule Diary.D.Day do
   use Ecto.Schema
   import Ecto.Changeset
-  alias Diary.D.Day
+  alias Diary.D.{Day, Todo}
 
 
   schema "days" do
     field :date, :naive_datetime
     field :diary_entry, :string
     field :user_id, :id
+
+    has_many :todos, Todo
 
     timestamps()
   end
--- lib/diary/d/user.ex
+++ lib/diary/d/user.ex
@@ -1,12 +1,15 @@
 defmodule Diary.D.User do
   use Ecto.Schema
   import Ecto.Changeset
-  alias Diary.D.User
+  alias Diary.D.{User, Day}
 
 
   schema "users" do
     field :email, :string
+    field :password, :string, virtual: true
     field :password_hash, :string
+
+    has_many :days, Day
 
     timestamps()
   end
